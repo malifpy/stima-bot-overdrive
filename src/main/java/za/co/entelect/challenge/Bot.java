df7632e3c40018bcfb6a863176340eb160cfb759
Fitrah-16520207 <fitnug30@gmail.com>
2022-02-14 11:07:41 +0700

    Update Bot.java

--- src/main/java/za/co/entelect/challenge/Bot.java
+++ src/main/java/za/co/entelect/challenge/Bot.java
@@ -33,7 +33,7 @@ public class Bot {
     }
 
     public Command run() {
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block,"mid");
         for (PowerUps powerUp: myCar.powerups) {
             if (powerUp.equals(PowerUps.BOOST)) {
                 boost = true;
@@ -59,22 +59,38 @@ public class Bot {
             return new AccelerateCommand();
         }
         if (blocks.contains(Terrain.MUD) | blocks.contains(Terrain.OIL_SPILL) | blocks.contains(Terrain.WALL) ) {
+
             if(myCar.position.lane==1){
+                List<Object> blocksRight = getBlocksInFront((myCar.position.lane)+1, myCar.position.block,"right");
+                if(blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL) & lizard ){
+                    return new LizardCommand();
+                }
                 return new ChangeLaneCommand(directionList.get(1));
             }
             else if(myCar.position.lane==4){
+
+                List<Object> blocksLeft = getBlocksInFront((myCar.position.lane)-1, myCar.position.block,"left");
+                if(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL)  & lizard ){
+                    return new LizardCommand();
+                }
                 return new ChangeLaneCommand(directionList.get(0));
             }
             else {
-                List<Object> blocksLeft = getBlocksInFront((myCar.position.lane)-1, myCar.position.block);
-                List<Object> blocksRight = getBlocksInFront((myCar.position.lane)+1, myCar.position.block);
+                List<Object> blocksLeft = getBlocksInFront((myCar.position.lane)-1, myCar.position.block,"left");
+                List<Object> blocksRight = getBlocksInFront((myCar.position.lane)+1, myCar.position.block,"right");
+                if(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL) & !(blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL)) ){
+                    return new ChangeLaneCommand(directionList.get(1));
+                }
+                if(blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL) & !(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL))){
+                    return new ChangeLaneCommand(directionList.get(0));
+                }
                 if(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL) & blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL) & lizard ){
                     return new LizardCommand();
                 }
                 if(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL) & blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL)){
-                    int obsLeft = hitungObstacle((myCar.position.lane)-1,myCar.position.block);
-                    int obsRight = hitungObstacle((myCar.position.lane)+1,myCar.position.block);
-                    int obsNow = hitungObstacle((myCar.position.lane),myCar.position.block);
+                    int obsLeft = hitungObstacle((myCar.position.lane)-1,myCar.position.block,"left");
+                    int obsRight = hitungObstacle((myCar.position.lane)+1,myCar.position.block,"right");
+                    int obsNow = hitungObstacle((myCar.position.lane),myCar.position.block,"mid");
                     if(obsNow<=obsRight & obsNow <= obsLeft){
                         return new AccelerateCommand();
                     }
@@ -86,23 +102,18 @@ public class Bot {
                     }
 
                 }
-                if(blocksLeft.contains(Terrain.MUD) | blocksLeft.contains(Terrain.OIL_SPILL) | blocksLeft.contains(Terrain.WALL) ){
-                    return new ChangeLaneCommand(directionList.get(1));
-                }
-                if(blocksRight.contains(Terrain.MUD) | blocksRight.contains(Terrain.OIL_SPILL) | blocksRight.contains(Terrain.WALL) ){
-                    return new ChangeLaneCommand(directionList.get(0));
-                }
+
                 int i = random.nextInt(directionList.size());
                 return new ChangeLaneCommand(directionList.get(i));
             }
         }
-        if (boost) {
+        if (boost & cekBoost((myCar.position.lane),myCar.position.block)) {
             return new BoostCommand();
         }
-        if (emp) {
+        if (emp & myCar.speed == maxSpeed) {
             return new EmpCommand();
         }
-        if (oil) {
+        if (oil & myCar.speed == maxSpeed) {
             return new OilCommand();
         }
         return new AccelerateCommand();
@@ -112,17 +123,31 @@ public class Bot {
      * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
      * traversed at max speed.
      **/
-    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
-
-        return false;
+    private Boolean cekBoost(int lane, int block) {
+        List<Lane[]> map = gameState.lanes;
+        int startBlock = map.get(0)[0].position.block;
+        Lane[] laneList = map.get(lane - 1);
+        for (int i = max(block - startBlock, 0); i <= block - startBlock + 15; i++) {
+            if (laneList[i].terrain == Terrain.MUD | laneList[i].terrain == Terrain.WALL | laneList[i].terrain == Terrain.OIL_SPILL) {
+                return false;
+            }
+        }
+        return true;
     }
-    private List<Object> getBlocksInFront(int lane, int block) {
+    private List<Object> getBlocksInFront(int lane, int block, String dirr) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
-
+        int start = max(block - startBlock, 0);
+        int end=block - startBlock + myCar.speed;
         Lane[] laneList = map.get(lane - 1);
-        for (int i = max(block - startBlock, 0); i <= block - startBlock + myCar.speed; i++) {
+        if (dirr.equalsIgnoreCase("left") | dirr.equalsIgnoreCase("right")){
+            end = end-1;
+        }
+        else{
+            start = start+1;
+        }
+        for (int i = start; i <= end; i++) {
             if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
                 break;
             }
@@ -132,13 +157,21 @@ public class Bot {
         }
         return blocks;
     }
-    private int hitungObstacle(int lane, int block) {
+    private int hitungObstacle(int lane, int block,String dirr) {
         List<Lane[]> map = gameState.lanes;
         int jumlah = 0;
         int startBlock = map.get(0)[0].position.block;
 
+        int start = max(block - startBlock, 0);
+        int end=block - startBlock + myCar.speed;
         Lane[] laneList = map.get(lane - 1);
-        for (int i = max(block - startBlock, 0); i <= block - startBlock + myCar.speed; i++) {
+        if (dirr.equalsIgnoreCase("left") | dirr.equalsIgnoreCase("right")){
+            end = end-1;
+        }
+        else{
+            start = start+1;
+        }
+        for (int i = start; i <= end; i++){
             if(laneList[i].terrain  == Terrain.MUD | laneList[i].terrain  == Terrain.WALL | laneList[i].terrain == Terrain.OIL_SPILL){
                 jumlah++;
             }
